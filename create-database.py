898e2389f77292cdfc59b591ddbcb5aca20942e4
Sawyer Czupka <sczupka@email.wm.edu>
2023-07-03 10:06:47 -0400

    First commit

---
+++ create-database.py
@@ -0,0 +1,23 @@
+# create-database.py
+# This script is used for taking a database of plaintext documents and making a vector database out of them.
+
+
+
+
+
+
+
+
+
+def main():
+    
+
+
+    
+    
+    
+     
+    pass
+
+if __name__ ==                   "__main__":
+    main()
