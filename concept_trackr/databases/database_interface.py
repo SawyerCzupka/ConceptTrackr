dca4519f694448ec30e6ef8fa1aeb8ff05c488a6
Sawyer Czupka <sczupka@email.wm.edu>
2023-07-21 18:07:26 -0400

    Cleaned up file

--- concept_trackr/databases/database_interface.py
+++ concept_trackr/databases/database_interface.py
@@ -4,60 +4,40 @@ This file contains all the logic for interacting with a Qdrant database and embe
 from typing import List
 
 from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 from sentence_transformers import SentenceTransformer
-from transformers import AutoTokenizer
-from dotenv import load_dotenv
-import os
-import logging
 
 
 class QdrantDatabase:
     def __init__(self, hostname='localhost', port=6333, collection='TEMP', embedding_model='all-mpnet-base-v2',
-                 chunk_size=384, chunk_overlap=0):
-        self.host = hostname
-        self.port = port
+                 chunk_size=384, chunk_overlap=0, embedding_size=768):
+        self._host = hostname
+        self._port = port
         self.collection = collection
 
-        # DOTENV
-        # load_dotenv()
-        # embedding_model = os.getenv("EMBEDDING_MODEL")
+        self.client = QdrantClient(self._host, port=self._port)
 
-        self.client = QdrantClient(self.host, port=self.port)
-
-        self.chunk_size = 128
+        self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
-        self.vector_size = 768
 
         self.model = SentenceTransformer(embedding_model, device='cuda')
-        self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/' + embedding_model)
+        self.vector_size = embedding_size
 
-        # self.splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-        #     self.model.tokenizer,
-        #     chunk_size=self.chunk_size,
-        #     chunk_overlap=self.chunk_overlap
-        # )
-
-        self.splitter = RecursiveCharacterTextSplitter(
-            chunk_size=384,
-            chunk_overlap=100,
-            length_function=self.len_func
-        )
-
-        # self.splitter = SentenceTransformersTokenTextSplitter(chunk_overlap=0,
-        #                                                       model_name='sentence-transformers/' + embedding_model,
-        #                                                       tokens_per_chunk=128)
+        self.splitter = self._get_splitter()
 
         self.nextID = 0
 
-    def len_func(self, text):
-        length = len(self.tokenizer(text).get('input_ids'))
-        return length
+    def _get_splitter(self):
+        return RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+            self.model.tokenizer,
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap
+        )
 
-    def encode(self, text):
-        return self.model.encode(text)
+    def _encode(self, text, **kwargs):
+        return self.model.encode(text, **kwargs)
 
     def create_collection(self):
         self.client.recreate_collection(
@@ -69,41 +49,35 @@ class QdrantDatabase:
         self.create_collection()
 
         chunks = self.chunk_documents(documents)
-        embeds = self.model.encode([chunk.page_content for chunk in chunks], show_progress_bar=True)
+        embeds = self._encode([chunk.page_content for chunk in chunks], show_progress_bar=True)
 
         self.add_elements(chunks, embeds)
 
     def chunk_documents(self, documents) -> List[Document]:
-        max_len = max([self.len_func(document.page_content) for document in documents])
-        logging.info(f"Chunk_documents | Input Length: {len(documents)}, MaxLength: {max_len}")
         results = self.splitter.split_documents(documents)
-        max_len = max([self.len_func(result.page_content) for result in results])
-        logging.info(f"Chunk_documents | Output Length: {len(results)}, MaxLength: {max_len}")
         return results
 
-    def getID(self):
+    def _getID(self):
         ID = self.nextID
         self.nextID += 1
         return ID
 
     def add_elements(self, documents: List[Document], embedding_vectors):
-        documents[0].metadata.update()
-
         self.client.upsert(
             collection_name=self.collection,
             points=[
                 models.PointStruct(
-                    id=self.getID(),
-                    payload={**documents[i].metadata, **{'page_content': documents[i].page_content}},
+                    id=self._getID(),
+                    payload={**document.metadata, **{'page_content': document.page_content}},
                     vector=embedding_vectors[i].tolist(),
-                ) for i in range(len(documents))
+                ) for i, document in enumerate(documents)
             ]
         )
 
     def search(self, text_query, **kwargs):
         results = self.client.search(
             collection_name=self.collection,
-            query_vector=self.encode(text_query).tolist(),
+            query_vector=self._encode(text_query).tolist(),
             **kwargs
         )
 
