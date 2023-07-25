7c2f441aef98462ab86cd49bdd4758b6d64e3692
Sawyer Czupka <sczupka@email.wm.edu>
2023-07-25 10:52:26 -0400

    Added main class interface skeleton

--- concept_trackr/concepttrackr.py
+++ concept_trackr/concepttrackr.py
@@ -1,4 +1,16 @@
+"""
+This class serves as the main interface for this package. It consolidates all the features into an
+easy-to-use class that takes care of all setup.
+"""
 
 
 class ConceptTrackr:
-    pass
+
+    def countOccurrences(self):
+        pass
+
+    def answerQuestion(self, question, projectID):
+        pass
+
+    def extractContext(self, issue):
+        pass
