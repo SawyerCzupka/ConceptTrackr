f2be4bb52cde9fdcb5243120aa76a89a188ac669
SawyerCzupka <sawyerczupka@gmail.com>
2023-08-22 14:22:31 -0400

    Added first draft of spreadsheet questions

---
+++ gef_analyzr/prompts/gef_questions.py
@@ -0,0 +1,36 @@
+SES_DESC_PROP = (
+    "Give a short description of any SES goals stated in the project proposal"
+)
+
+SES_DESC_ANY = (
+    "Give a short description of any SES goals stated anywhere in the project"
+)
+
+SES_CO = "Give a short description of possible SES co-benefits of the project"
+
+PROJ_SUCCESS = "Was the project a success or failure? Why?"
+
+POP_IMPACT = "What populations might be impacted by this project?"
+
+SES_INIT = "Did the project have SES in the initial report?"
+
+SES_CLOSE = "Did the project report SES impacts in the closeout?"
+
+CONSEQUENCES = "Were there unintended consequences of the project?"
+
+GENDER = "Were there benefits in SES for gender or women? If so what kind of effects?"
+
+GOAL = "What was the overall environmental goal of the project?"
+
+SPREADSHEET_QUESTIONS = [
+    SES_DESC_PROP,
+    SES_DESC_ANY,
+    SES_CO,
+    PROJ_SUCCESS,
+    POP_IMPACT,
+    SES_INIT,
+    SES_CLOSE,
+    CONSEQUENCES,
+    GENDER,
+    GOAL,
+]
