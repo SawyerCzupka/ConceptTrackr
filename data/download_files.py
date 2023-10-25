7b9b7b43e31c8ae9a61c43d7421bf40fae4b9b40
Sawyer <sawyerczupka@gmail.com>
2023-10-24 22:39:53 -0400

    updated download script

--- data/download_files.py
+++ data/download_files.py
@@ -1,14 +1,20 @@
 import os
 import csv
 import requests
+import logging
 from bs4 import BeautifulSoup
 import dask
-from dask import delayed, compute
+from dask import delayed
 
 PROJECTS_CSV_PATH = "projects.csv"
 BASE_URL = "https://www.thegef.org/projects-operations/projects/"
 OUTPUT_PATH = "dump"
 INTERESTED_YEARS = [i for i in range(2012, 2024)]
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def get_project_ids_from_csv(path):
@@ -16,64 +22,54 @@ def get_project_ids_from_csv(path):
     with open(path, "r") as csvfile:
         reader = csv.reader(csvfile)
         next(reader)  # Skip the header row
-
         for row in reader:
             try:
-                year = int(row[9])  # Assuming the year is in the 10th column
+                year = int(row[9])
                 if year in INTERESTED_YEARS:
-                    project_id = row[1]  # Assuming the ID is in the 2nd column
+                    project_id = row[1]
                     project_ids.append(project_id)
-            except:
+            except ValueError:
                 continue
     return project_ids
 
 
-@delayed
-def download_pdf(link, project_id, pdf_counter):
-    href = link.get("href")
-    if href and ".pdf" in href:
-        try:
-            pdf_response = requests.get(href)
-            pdf_file_path = os.path.join(
-                OUTPUT_PATH, f"pdf{project_id}_{pdf_counter}.pdf"
-            )
-            with open(pdf_file_path, "wb") as pdf_file:
-                pdf_file.write(pdf_response.content)
-            return f"Downloaded file {pdf_counter}"
-        except Exception as e:
-            return f"Failed to download PDF from: {href}", e
-
-
-@delayed
 def download_pdfs_from_project_page(project_id):
     url = BASE_URL + str(project_id)
     response = requests.get(url)
-
     if response.status_code != 200:
-        return f"Failed to access website: {url}"
+        logging.warning(f"Failed to access website: {url}")
+        return []
 
     soup = BeautifulSoup(response.text, "html.parser")
     links = soup.find_all("a")
+    pdf_links = [
+        link.get("href")
+        for link in links
+        if link.get("href") and ".pdf" in link.get("href")
+    ]
 
-    results = []
-    pdf_counter = 0
-    for link in links:
-        pdf_counter += 1
-        result = download_pdf(link, project_id, pdf_counter)
-        results.append(result)
+    downloaded_files = []
+    for idx, href in enumerate(pdf_links):
+        try:
+            pdf_response = requests.get(href)
+            pdf_file_path = os.path.join(OUTPUT_PATH, f"pdf{project_id}_{idx}.pdf")
+            with open(pdf_file_path, "wb") as pdf_file:
+                pdf_file.write(pdf_response.content)
+            logging.info(f"Downloaded file: {pdf_file_path}")
+            downloaded_files.append(pdf_file_path)
+        except Exception as e:
+            logging.error(f"Failed to download PDF from: {href}. Error: {e}")
 
-    return results
+    return downloaded_files
 
 
 def main():
+    if not os.path.exists(OUTPUT_PATH):
+        os.makedirs(OUTPUT_PATH)
+
     project_ids = get_project_ids_from_csv(PROJECTS_CSV_PATH)
-
-    tasks = [download_pdfs_from_project_page(pid) for pid in project_ids]
-    results = compute(*tasks)  # This will parallelly compute the tasks
-
-    for result in results:
-        for msg in result:
-            print(msg)
+    tasks = [delayed(download_pdfs_from_project_page)(pid) for pid in project_ids]
+    _ = dask.compute(*tasks)
 
 
 if __name__ == "__main__":
