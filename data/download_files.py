7bb764f83e76aa210f637e9cfe65c2d0cfdaee3d
Sawyer <sawyerczupka@gmail.com>
2023-10-24 22:12:50 -0400

    updated download script

--- data/download_files.py
+++ data/download_files.py
@@ -1,78 +1,62 @@
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-import openpyxl
 import os
+import requests
+import openpyxl
+from bs4 import BeautifulSoup
 
-path = "projects.xlsx"
-# First download Excel spreadsheet off of GEF database:
-# https://www.thegef.org/projects-operations/database --> scroll down left side --> export data in CSV format --> save in same folder as this
-# IMPORTANT: save that file as an Excel spreadsheet instead of its original CSV (Comma Separated Values) format
-wb_obj = openpyxl.load_workbook(path)
-sheet_obj = wb_obj.active
-row = sheet_obj.max_row
-column = sheet_obj.max_column
-
-url_base = "https://www.thegef.org/projects-operations/projects/"
-# Base url for all GEF projects
-print(url_base)
-
-base_path = r"NEW"
+PROJECTS_SPREADSHEET_PATH = "projects.xlsx"
+BASE_URL = "https://www.thegef.org/projects-operations/projects/"
+OUTPUT_PATH = "NEW"
+INTERESTED_YEARS = [i for i in range(2012, 2024)]
 
 
-def read():
-    # Specific to Excel spreadsheet, change row/ column parameters to download files based off of country, region, etc.
-    ls = []
-    for i in range(1, row + 1):
-        cell_obj = sheet_obj.cell(row=i, column=10)
-        if cell_obj.value == 2015:  # year of interested projects (CHANGE)
-            cell_obj1 = sheet_obj.cell(
-                row=i, column=2
-            )  # ID column from Excel spreadsheet
-            ls.append(cell_obj1.value)
-    return ls
+def get_project_ids_from_spreadsheet(path):
+    workbook = openpyxl.load_workbook(path)
+    sheet = workbook.active
+
+    project_ids = []
+
+    for row in range(1, sheet.max_row + 1):
+        year_cell = sheet.cell(row=row, column=10)
+        if year_cell.value in INTERESTED_YEARS:
+            id_cell = sheet.cell(row=row, column=2)
+            project_ids.append(id_cell.value)
+
+    return project_ids
+
+
+def download_pdfs_from_project_page(project_id):
+    url = BASE_URL + str(project_id)
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Failed to access website: {url}")
+        return
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all("a")
+
+    pdf_counter = 0
+    for link in links:
+        href = link.get("href")
+        if href and ".pdf" in href:
+            pdf_counter += 1
+            try:
+                pdf_response = requests.get(href)
+                pdf_file_path = os.path.join(
+                    OUTPUT_PATH, f"pdf{project_id}_{pdf_counter}.pdf"
+                )
+                with open(pdf_file_path, "wb") as pdf_file:
+                    pdf_file.write(pdf_response.content)
+                print(f"Downloaded file {pdf_counter}")
+            except Exception as e:
+                print(f"Failed to download PDF from: {href}")
+                print(e)
 
 
 def main():
-    # list = read()
-    list = [4751, 10255]
-    for (
-        i
-    ) in (
-        list
-    ):  # Uses list just created of all the IDs for year specified, change to (1, 11304) to download ALL GEF projects
-        url = url_base + str(i)
-        response = requests.get(url)
-        if response.status_code == 200:
-            print(url + " Web site exists")
-            response = requests.get(url)
-            # Parse text
-            soup = BeautifulSoup(response.text, "html.parser")
-            # Find all hyperlinks on webpage
-            links = soup.find_all("a")
-            g = 0
-
-            for link in links:
-                try:
-                    if ".pdf" in link.get("href", []):
-                        g += 1
-                        response = requests.get(link.get("href"))
-                        pdf = open(
-                            os.path.join(
-                                base_path, "pdf" + str(i) + "_" + str(g) + ".pdf"
-                            ),
-                            "wb",
-                        )  # Prints GEF pdfs as #ID_#file of project
-                        pdf.write(response.content)
-                        pdf.close()
-                        # print(f"Link: {link}")
-                        print("File ", g, " downloaded")
-                except Exception as e:
-                    print("pdf does not exist")
-                    print(e)
-
-        else:
-            print("Web site does not exist")
+    project_ids = get_project_ids_from_spreadsheet(PROJECTS_SPREADSHEET_PATH)
+    for project_id in project_ids:
+        download_pdfs_from_project_page(project_id)
 
 
 if __name__ == "__main__":
